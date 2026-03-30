f1bc220cd1a2b62c50d980509523a1bb5368de1b
khushaliPrajapati <khushali@groottechnologies.com>
2026-03-30 12:09:48 +0530

    order caculation added

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,29 +1,21 @@
 import express from 'express';
 import {
     createOrder, getMyOrders, respondToOrder, getAllOrders,
-<<<<<<< HEAD
     getShopOrderStats, getAllRequests, updateOrder, deleteOrder,
-    assignDeliveryAndNotify
-=======
-    getShopActiveOrders, getAllRequests, updateOrder, deleteOrder,
-    assignDeliveryAndNotify, calculatePrice
->>>>>>> c14c409 (order calculate payment)
+    assignDeliveryAndNotify, getShopActiveOrders, calculatePrice
 } from "../controllers/orderController.js";
 import { authMiddleware, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createOrder);
-<<<<<<< HEAD
 router.get("/OrderHistory", authMiddleware, getMyOrders);
 router.get("/all", authMiddleware, getAllRequests); // Using getAllRequests for the 'All' view
 router.get("/shop/stats", authMiddleware, getShopOrderStats);
-=======
 router.post("/calculate-price", authMiddleware, calculatePrice);
 router.get("/OrderHistory", authMiddleware, getMyOrders);
 router.get("/all", authMiddleware, getAllOrders);
 router.get("/shop/active", authMiddleware, getShopActiveOrders);
->>>>>>> c14c409 (order calculate payment)
 router.put("/action", authMiddleware, respondToOrder);
 router.get("/requests", authMiddleware, getAllRequests);
 
